a6d5034b74e04900c3dd3ad4d4399e65eb26a8a5
Nissan Dookeran <nissan.dookeran@gmail.com>
2021-12-29 05:43:05 +1000

    adds customisations

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,9 @@ import { useWeb3 } from "@3rdweb/hooks";
 
 import { ThirdwebSDK } from "@3rdweb/sdk";
 
+import { UnsupportedChainIdError } from "@web3-react/core";
+
+
 // We instantiate the sdk on Rinkeby.
 const sdk = new ThirdwebSDK("rinkeby");
 
@@ -181,6 +184,18 @@ const App = () => {
       });
 
   }, [address]);
+
+  if (error instanceof UnsupportedChainIdError ) {
+    return (
+      <div className="unsupported-network">
+        <h2>Please connect to Rinkeby</h2>
+        <p>
+          This dapp only works on the Rinkeby network, please switch networks
+          in your connected wallet.
+        </p>
+      </div>
+    );
+  }
 
   // This is the case where the user hasn't connected their wallet
   // to your web app. Let them call connectWallet.
